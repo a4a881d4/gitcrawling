4e1dc59d735b5bfb9b6372b0ffdcd2c8d11658d7
a4a881d4 <a4a881d4@163.com>
2020-03-04 00:26:54 +0800

    open

--- ref/open/main.go
+++ ref/open/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"io"
 
 	. "github.com/a4a881d4/gitcrawling/ref"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
 // Basic example of how to clone a repository using clone options.
@@ -25,4 +27,28 @@ func main() {
 		fmt.Println(t.Type())
 		return nil
 		})
+
+	ref, err := r.Head()
+	CheckIfError(err)
+
+	tree,err := r.TreeObject(ref.Hash())
+	CheckIfError(err)
+
+	seen := make(map[plumbing.Hash]bool)
+	walker := object.NewTreeWalker(tree,false,seen)
+
+	for {
+		name, entry, err := walker.Next()
+		if _,ok := seen[entry.Hash]; ok {
+			continue
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		fmt.Printf("name:%s hash:%x mode:%s\n",name,entry.Hash,entry.Mode.String())
+	}
 }
