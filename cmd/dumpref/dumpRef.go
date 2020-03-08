9fe6f9ce77e7ae2c17b1f6f11c43c4cd243bede2
a4a881d4 <a4a881d4@163.com>
2020-03-08 10:35:00 +0800

    rm db resouce

--- cmd/dumpref/dumpRef.go
+++ cmd/dumpref/dumpRef.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/a4a881d4/gitcrawling/db"
 )
@@ -34,7 +35,6 @@ func main() {
 		return
 	}
 	for k,v := range rec {
-		fmt.Println("Process",k,"stars")
 		for _,name := range v {
 			repo := strings.Split(name,"/")
 			if len(repo)!=2 {
@@ -45,14 +45,30 @@ func main() {
 			path := fmt.Sprintf("%s/repos/%s/%s",*argReposDir,owner,project)
 			_, err := os.Stat(path)
 			if err!=nil {
-				fmt.Println(owner,project,"miss")
+				fmt.Println(ShowName(owner,project),"miss")
 			}
 			if rdb.IsBuild(owner,project) {
-				fmt.Println(owner,project,"has build")
+				fmt.Println(ShowName(owner,project),"has build")
 			}
 			if rdb.OK(owner,project) {
-				fmt.Println(owner,project,"clone local")
+				fmt.Println(ShowName(owner,project),"clone local")
 			}
+			<- time.After(time.Second*1)
 		}
 	}
+}
+
+func ShowName(owner,project string) string {
+	var space = "                                                                        "
+	if len(owner) > 15 {
+		owner = owner[:15]
+	} else {
+		owner = ret[:15-len(owner)]+owner
+	}
+	if len(project) > 25 {
+		project = project[:15]
+	} else {
+		project = project + ret[:25-len(project)]
+	}
+	return owner+":"+project
 }
