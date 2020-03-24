a6d0d37ead183c6e4bbc4b363db1ebe40d5751e3
a4a881d4 <a4a881d4@163.com>
2020-03-25 02:37:58 +0800

    new pack dir

--- utils/clearpack.py
+++ utils/clearpack.py
@@ -8,9 +8,9 @@ fo = open(sys.argv[1])
 for line in fo.readlines():
     line = line.strip().split("/")
     if len(line[0])>2:
-        dir = "packs/"+line[0][:2]
+        dir = sys.argv[2]+"/packs/"+line[0][:2]
     else:
-        dir = "packs/"+line[0]
+        dir = sys.argv[2]+"/packs/"+line[0]
     dir += "/"+"/".join(line)
     if os.path.exists(dir):
         if os.path.exists(dir+"/tmp-pack"):
--- utils/clonePack.sh
+++ utils/clonePack.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 ps -ef|grep "uclone" | grep $1 |grep -v grep|cut -c 9-15|xargs kill -9
 wc -l ./task/$1/miss/miss
-python3 ./bin/clearpack.py ./task/$1/miss/miss
-nohup ./bin/uclone -m ./task/$1/miss -t 32 > $1.log &
+python3 ./bin/clearpack.py ./task/$1/miss/miss $1
+nohup ./bin/uclone -r $1 -m ./task/$1/miss -t 32 > $1.log &
