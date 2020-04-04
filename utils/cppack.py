b22d8f56e13d2e2a56023e1b794c001a57daa3ee
a4a881d4 <a4a881d4@163.com>
2020-04-05 02:40:48 +0800

    cppack

--- utils/cppack.py
+++ utils/cppack.py
@@ -4,7 +4,7 @@ import os
 
 for root, dirs, files in os.walk(sys.argv[1], topdown=False):
     for name in files:
-        if "pack" in name:
+        if "pack-" in name and (".pack" in name or ".idx" in name):
             fn = os.path.join(root, name)
             shutil.copy(fn,sys.argv[2])
             print(fn)
