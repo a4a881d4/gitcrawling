002f1f1b51358b4359371ed529170765f5774ea6
a4a881d4 <a4a881d4@163.com>
2020-04-19 01:35:37 +0800

    ls

--- utils/first/ls.py
+++ utils/first/ls.py
@@ -8,14 +8,10 @@ def parsekv(k,ref):
     return "/".join(rs),ref['Hash']
 
 odb = db.LevelDB(sys.argv[1])
-rdb = db.LevelDB(sys.argv[2])
-batch = db.WriteBatch()
 
 for k,v in odb.RangeIter(key_from=str.encode('r/')):
     vo = json.loads(v)
     for ref in vo['Refs']:
         ref,h = parsekv(k.decode(),ref)
-        batch.Put(str.encode('refs/'+h),str.encode(ref))
-        batch.Put(str.encode('proj/'+ref),str.encode(h))
+        
         print(ref,h)
-rdb.Write(batch)
