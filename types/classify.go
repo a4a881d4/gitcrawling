219b30d2a496dd385db7e5b96e2ea8b22abadfb8
a4a881d4 <a4a881d4@163.com>
2020-04-19 17:57:26 +0800

    pending

--- types/classify.go
+++ types/classify.go
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 package types
 
 type Classifer interface {
 	Hit(Hash) bool
 	NamePrefix() string
 }
-=======
-package types
-
-type Classifer interface {
-	Hit(Hash) bool
-	FileNamePrefix() string
-}
->>>>>>> 002f1f1b51358b4359371ed529170765f5774ea6
